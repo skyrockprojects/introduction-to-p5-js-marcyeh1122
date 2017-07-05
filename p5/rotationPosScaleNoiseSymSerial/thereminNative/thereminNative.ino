47aaacad3160eaa6a383bf36ac37de6a68ef4f9f
J-Fo-S <jonathan@skyrockprojects.com>
2017-07-05 14:05:02 +0800

    Update thereminNative.ino

--- p5/rotationPosScaleNoiseSymSerial/thereminNative/thereminNative.ino
+++ p5/rotationPosScaleNoiseSymSerial/thereminNative/thereminNative.ino
@@ -1,14 +1,10 @@
 // defines pins numbers
 const int trig = 9;
 const int echo = 10;
-//int freq;
-//const int ledPin = 13;
 
 // defines variables
 long duration;
 int distance;
-int safetyDistance;
-
 
 
 void setup() {
@@ -21,9 +17,7 @@ void setup() {
 
 
 void loop() {
-  
-
-  
+    
   // Clears the trigPin
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
@@ -39,88 +33,6 @@ void loop() {
   // Calculating the distance
   distance = duration * 0.034 / 3;
 
-  //safetyDistance = distance;
-
-  //freq = distance;
-  /*int speaker = map(freq, 0, 11, 10, 1000);
-
-  if (distance == 2)
-  {
-    tone(sound, 261.63);
-  } //c
-
-  if (distance == 4)
-  {
-    tone(sound, 293.66);
-  } //d
-
-  if (distance == 6)
-  {
-    tone(sound, 329.63);
-  } //e
-
-  if (distance == 8)
-  {
-    tone(sound, 349.23);
-  } //f
-
-  if (distance == 10)
-  {
-    tone(sound, 392);
-  } //g
-
-  if (distance == 12)
-  {
-    tone(sound, 440);
-  } //a
-
-  if (distance == 14)
-  {
-    tone(sound, 466.16);
-  } //Bb
-
-  if (distance == 16)
-  {
-    tone(sound, 523.25);
-  } //Bb
-
-  if (distance == 18)
-  {
-    tone(sound, 587.33);
-  }
-
-  if (distance == 20)
-  {
-    tone(sound, 659.25);
-  }
-
-  if (distance == 22)
-  {
-    tone(sound, 698.46);
-  }
-
-
-  if (distance == 24)
-  {
-    tone(sound, 880.00);
-  }
-  
-
-  if (distance > 26)
-  {
-    tone(sound, LOW);
-  }
-
-
-  //tone(sound, speaker);*/
-
-
-  /*if (safetyDistance <= 5){
-    digitalWrite(sound, HIGH);
-    }
-    else{
-    digitalWrite(sound, LOW);
-    } */
 
   // Prints the distance on the Serial Monitor
  //Serial.print("Distance: ");
@@ -128,12 +40,3 @@ void loop() {
  Serial.write(distance);
 }
 
-
-
-//http://www.music-for-music-teachers.com/o-mio-babbino.html
-
-//http://www.phy.mtu.edu/~suits/notefreqs.html
-
-//https://github.com/drios42/toneInClassFeb2017/blob/master/tone.ino
-
-//https://www.youtube.com/watch?v=HynLoCtUVtU
