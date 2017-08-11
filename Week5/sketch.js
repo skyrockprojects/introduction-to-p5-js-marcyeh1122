fed8ee437db417c64b28a879272cf6df5883b2e3
vinsis <sisodia.vinay@gmail.com>
2017-08-11 14:27:35 +0800

    Week 5 creations

--- Week5/sketch.js
+++ Week5/sketch.js
@@ -1,8 +1,32 @@
+var x = 400;
+var y = 400;
+var direction = 1;
+var directionY = 1;
+
 function setup(){
-  createCanvas(600,600);
-  background(200);
+  createCanvas(500,500);
+    
 }
 
 function draw(){
-
+    
+    background("gray")
+    fill("black");
+    
+    if(x == 500){
+        direction = -1;
+    }
+    if(y == 0) {
+        directionY = -1;
+    }
+    if(x == 0) {
+        direction = 1;
+    }
+    if (y == 500) {
+        directionY = 1;
+    }
+    
+    x = x + direction;
+    y = y - directionY;
+    ellipse(x,y,50,50);
 }
