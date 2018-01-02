bbedd40dcde3f50f3bcaf96f594dad41ec863d9d
vinsis <6437295+vinsis@users.noreply.github.com>
2018-01-02 09:00:19 +0800

    removed code from sketch

--- first-p5-project/sketch.js
+++ first-p5-project/sketch.js
@@ -1,24 +1,7 @@
 function setup(){
-  createCanvas(500,500);
-
-  for(var i = 0; i < 9; i=i+1)
-  {
-    for (var j = 0; j < 9; j++){
-      if ((i+j)%2 == 1){
-        fill("black")
-      } else {
-        fill("white")
-      }
-      rect(50*i,50*j,50,50);
-    }
-
-  }
-
 
 }
 
 function draw(){
-
-
 
 }
