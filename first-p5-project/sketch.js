08fe878eb861ea0d259cb044567aabf7a7eb83c9
marcyeh1122 <31614347+marcyeh1122@users.noreply.github.com>
2018-02-07 18:05:58 +0800

    Smiley Face

--- first-p5-project/sketch.js
+++ first-p5-project/sketch.js
@@ -1,19 +1,11 @@
-function setup(){
-    createCanvas(800, 600);
-    background(0);
-    
-    stroke(200);
-    fill(200);
-    rect(100,150,700,700);
-    rect(100,200,0,255)
-    
-    fill(255,0,0);
-    ellipse(100,20,200);
-    
-    stroke(0,255,0);
-    line(20, 50, 100, 250);
+Var radius;
+
+function setup() {
+    createCanvas(500,500);
+    background(255);
+    radius = 0;
 }
 
-function draw(){
+function draw() {
 
 }
